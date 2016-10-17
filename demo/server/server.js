5088d0d44475cbddb6dd04351057f9aa2adf6202
dianacheung <diana.cheung.2013@gmail.com>
2016-10-17 11:41:21 -0700

    added demo folder and moved client and server folders inside, edited npm start script path in package.json, updated paths in server.js

--- demo/server/server.js
+++ demo/server/server.js
@@ -8,11 +8,13 @@ app.set('port', process.env.PORT || 3000);
 // require in conspectio npm package and invoke
 // const conspectioServer = require('conspectio');
 // lib in progress, use the lib server conspectioServer.js
-const conspectioServer = require('./../lib/server/conspectioServer.js');
+// updated require path due to demo folder
+const conspectioServer = require('./../../lib/server/conspectioServer.js');
 conspectioServer(http);
 
 // currently don't have a CDN conspectio client lib, need this server to host the file
-app.use(express.static(path.join(`${__dirname}/../dist`)));
+// updated require path due to demo folder
+app.use(express.static(path.join(`${__dirname}/../../dist`)));
 
 // serve up demo client side files
 app.use(express.static(path.join(`${__dirname}/../client`)));
